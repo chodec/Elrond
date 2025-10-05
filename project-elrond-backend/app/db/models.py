02689d3da2e0c9d7e853c15fc564f81a72a9990e
dominik <dom.lepizmo@email.cz>
2025-10-05 18:11:15 +0200

    add more db logic + comments

--- project-elrond-backend/app/db/models.py
+++ project-elrond-backend/app/db/models.py
@@ -31,13 +31,15 @@ class Trainer(Base):
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), primary_key=True)
     specialization = Column(String, nullable=True)
     user = relationship("User", back_populates="trainer_profile")
-    #Can have N clients
+    # Can have N clients
     clients_trained = relationship(
         "Client",
         secondary="client_trainer_association",
         back_populates="trainers"
     )
     meal_plans_created = relationship("MealPlan", back_populates="creator")
+    exercise_plans_created = relationship("ExercisePlan", back_populates="creator") 
+
 
 
 class Client(Base):
@@ -51,11 +53,12 @@ class Client(Base):
         secondary="client_trainer_association",
         back_populates="clients_trained"
     )
-    assignments = relationship("MealPlanAssignment", back_populates="client")
+    meal_assignments = relationship("MealPlanAssignment", back_populates="client")
+    exercise_assignments = relationship("ExercisePlanAssignment", back_populates="client") 
 
-#M:N relationshiop between trainer and client
+
+# M:N relationshiop between trainer and client
 class ClientTrainerAssociation(Base):
-    """ Vazební tabulka N:M Klient k Trenérovi. """
     __tablename__ = "client_trainer_association"
     client_id = Column(UUID(as_uuid=True), ForeignKey("clients.user_id"), primary_key=True)
     trainer_id = Column(UUID(as_uuid=True), ForeignKey("trainers.user_id"), primary_key=True)
@@ -63,36 +66,99 @@ class ClientTrainerAssociation(Base):
     __table_args__ = (UniqueConstraint('client_id', 'trainer_id', name='uq_client_trainer_pair'),)
 
 
+# Trainer Meals
+# Flow: 
+# 1. Trainer creates meal
+# 2. Trainer creates plan
+# 3. Trainer add meal
+# 4. Trainer specifies meal details
+# 5. BE creates MealPlanEntry so the meals stay clean and shiny    
+
 class Meal(Base):
-    """ Základní definice jídla. """
     __tablename__ = "meals"
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     name = Column(String, index=True, nullable=False)
+
+
+class MealPlanEntry(Base):
+    __tablename__ = "meal_plan_entries"
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     
+    meal_plan_id = Column(UUID(as_uuid=True), ForeignKey("meal_plans.id"), index=True, nullable=False)
+    base_meal_id = Column(UUID(as_uuid=True), ForeignKey("meals.id"), index=True, nullable=False)
+    
+    serving_size_grams = Column(Integer, nullable=False)
+    time_slot = Column(String, nullable=False)          
+    notes = Column(String, nullable=True)               
+    
+    meal_plan = relationship("MealPlan", back_populates="meal_entries")
+    base_meal = relationship("Meal")
+
+
+class MealPlan(Base):
+    __tablename__ = "meal_plans"
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    name = Column(String, index=True, nullable=False)
+    trainer_id = Column(UUID(as_uuid=True), ForeignKey("trainers.user_id"), index=True, nullable=False) 
+    creator = relationship("Trainer", back_populates="meal_plans_created")
+    assignments = relationship("MealPlanAssignment", back_populates="meal_plan")
+    
+    meal_entries = relationship("MealPlanEntry", back_populates="meal_plan")
+
+
+class MealPlanAssignment(Base):
+    __tablename__ = "meal_plan_assignments"
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    client_id = Column(UUID(as_uuid=True), ForeignKey("clients.user_id"), nullable=False, index=True)
+    meal_plan_id = Column(UUID(as_uuid=True), ForeignKey("meal_plans.id"), nullable=False, index=True)
+    client = relationship("Client", back_populates="meal_assignments") 
+    meal_plan = relationship("MealPlan", back_populates="meal_plan")
+
+# Trainer exercise
+# Flow: 
+# 1. Trainer creates exercise
+# 2. Trainer creates plan
+# 3. Trainer add exercise
+# 4. Trainer specifies exercise details
+# 5. BE creates ExercisePlanEntry so the exercises stay clean and shiny    
 
 class Exercise(Base):
-    """ Základní definice cvičení. """
     __tablename__ = "exercises"
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     name = Column(String, index=True, nullable=False)
 
 
-class MealPlan(Base):
-    """ Definice jídelního plánu. """
-    __tablename__ = "meal_plans"
+class ExercisePlanEntry(Base):
+    __tablename__ = "exercise_plan_entries"
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    
+    exercise_plan_id = Column(UUID(as_uuid=True), ForeignKey("exercise_plans.id"), index=True, nullable=False) 
+    base_exercise_id = Column(UUID(as_uuid=True), ForeignKey("exercises.id"), index=True, nullable=False)
+    
+    sets = Column(Integer, nullable=False)            
+    repetitions = Column(String, nullable=False)         
+    order_in_session = Column(Integer, nullable=False) 
+    
+    exercise_plan = relationship("ExercisePlan", back_populates="exercise_entries")
+    base_exercise = relationship("Exercise")
+
+
+class ExercisePlan(Base):
+    __tablename__ = "exercise_plans"
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     name = Column(String, index=True, nullable=False)
     trainer_id = Column(UUID(as_uuid=True), ForeignKey("trainers.user_id"), index=True, nullable=False) 
-    meals = Column(ARRAY(UUID(as_uuid=True)), nullable=False) 
-    creator = relationship("Trainer", back_populates="meal_plans_created")
-    assignments = relationship("MealPlanAssignment", back_populates="meal_plan")
+    
+    creator = relationship("Trainer", back_populates="exercise_plans_created")
+    assignments = relationship("ExercisePlanAssignment", back_populates="exercise_plan")
+
+    exercise_entries = relationship("ExercisePlanEntry", back_populates="exercise_plan")
 
 
-class MealPlanAssignment(Base):
-    """ Vazební tabulka: Přiřazení plánu klientovi. """
-    __tablename__ = "meal_plan_assignments"
+class ExercisePlanAssignment(Base):
+    __tablename__ = "exercise_plan_assignments"
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     client_id = Column(UUID(as_uuid=True), ForeignKey("clients.user_id"), nullable=False, index=True)
-    meal_plan_id = Column(UUID(as_uuid=True), ForeignKey("meal_plans.id"), nullable=False, index=True)
-    client = relationship("Client", back_populates="assignments")
-    meal_plan = relationship("MealPlan", back_populates="meal_plan")
+    exercise_plan_id = Column(UUID(as_uuid=True), ForeignKey("exercise_plans.id"), nullable=False, index=True)
+    client = relationship("Client", back_populates="exercise_assignments")
+    exercise_plan = relationship("ExercisePlan", back_populates="assignments")
